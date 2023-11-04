993cada9d1d1bf5321dc22a846d70aa7e6f178db
siddht1 <rt1yrru+github@gmail.com>
2023-11-04 10:33:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -61,7 +61,7 @@ app.all("*", (req, res) => {
       
     // console.log({ type: 'json data', data: strippedStr });
      // parse_data(strippedStr);
-      res.json({stats:verify_data(strippedStr),length:strippedStr.length(),data:strippedStr});
+      res.json({stats:verify_data(strippedStr),data:strippedStr});
       console.log(strippedStr);
   } else {    
     res.json({ type: 'not a json data', data: data });    
